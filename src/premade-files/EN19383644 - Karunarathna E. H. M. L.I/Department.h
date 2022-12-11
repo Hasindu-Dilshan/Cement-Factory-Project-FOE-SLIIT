c36b80adb379ef0d8991da28f613ca344ab1025f
HasinduDilshan10 <dilshan.hasindu.10@gmail.com>
2022-12-12 00:08:17 +0530

    add premade files

---
+++ src/premade-files/EN19383644 - Karunarathna E. H. M. L.I/Department.h
@@ -0,0 +1,28 @@
+#include <string>
+using namespace std;
+
+const int maxMemberCount = 50;
+
+class Department{
+private:
+	static int idCount;
+
+protected:
+	int id;
+	string depName;
+
+public:
+	Department();
+	Department(string depName);	 
+	~Department();
+
+	void countDep();
+
+	// Getters
+	int getId();
+	string getName();
+
+	// Setters
+	void setId();
+	void setName(string depName);	
+};
